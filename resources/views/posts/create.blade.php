1342bce06a06b62ca47f91e35e5426a554258212
suigin0105 <suigin0105@gmail.com>
2023-07-02 14:29:10 +0900

    8-4

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body class="antialiased">
+        <h1>Blog Name</h1>
+        <form action="/posts" method="POST">
+            @csrf
+            <div class="title">
+              <h2>Title</h2>
+              <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}">
+              <a class='title_error' style='color:red'>{{ $errors->first('post.title') }}</a>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="ここに本文">{{ old('post.body') }}</textarea>
+                <a class="body_error" style="color:red">{{ $errors->first('post.body') }}</a>
+            </div>
+            <input type="submit" value="store"/>
+        </form>
+        <div class='footer'>
+            <a href="/">戻る</a>
+        </div>
+    </body>
+</html>
